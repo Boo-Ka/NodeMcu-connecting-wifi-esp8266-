c2ff9f3d7c6773f3862a39abd5c9c5a8f75c232f
Boopathi G <111436524+Boo-Ka@users.noreply.github.com>
2022-08-19 06:34:38 -0700

    Add files via upload

---
+++ smart_dust1.ino
@@ -0,0 +1,23 @@
+#include<ESP8266WiFi.h>
+#define ssid "1001001"
+#define password "1A2B3C4D"
+
+void setup() 
+{
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to ");
+  Serial.print(ssid);
+   while(WiFi.status() != WL_CONNECTED)
+  {
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println("Connected! IP address: ");
+  Serial.println(WiFi.localIP()); 
+}
+
+void loop()
+{
+
+}
